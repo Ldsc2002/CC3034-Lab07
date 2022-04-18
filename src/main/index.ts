a1244f5fab98d674d6744435212b60d054c26666
Ldsc2002 <luissantoscuellar@gmail.com>
2022-04-17 21:47:40 -0600

    Code cleanup

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,9 +2,9 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import { format as formatUrl } from "url";
 import installExtension, {
-  REDUX_DEVTOOLS,
-  REACT_DEVELOPER_TOOLS,
-  REACT_PERF,
+    REDUX_DEVTOOLS,
+    REACT_DEVELOPER_TOOLS,
+    REACT_PERF,
 } from "electron-devtools-installer";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
@@ -13,64 +13,60 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 let mainWindow: BrowserWindow | null;
 
 function createMainWindow() {
-  const window = new BrowserWindow({
-    webPreferences: { nodeIntegration: true },
-  });
-
-  if (isDevelopment) {
-    installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS, REACT_PERF])
-      .then((name) => console.log(`Added Extension:  ${name}`))
-      .catch((err) => console.log("An error occurred: ", err));
-
-    window.webContents.openDevTools();
-  }
-
-  if (isDevelopment) {
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
-  } else {
-    window.loadURL(
-      formatUrl({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file",
-        slashes: true,
-      })
-    );
-  }
-
-  window.on("closed", () => {
-    mainWindow = null;
-  });
-
-  window.webContents.on("devtools-opened", () => {
-    window.focus();
-    setImmediate(() => {
-      window.focus();
+    const window = new BrowserWindow({
+        webPreferences: { nodeIntegration: true },
     });
-  });
 
-  return window;
+    if (isDevelopment) {
+        installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS, REACT_PERF])
+            .then((name) => console.log(`Added Extension:  ${name}`))
+            .catch((err) => console.log("An error occurred: ", err));
+
+        window.webContents.openDevTools();
+    }
+
+    if (isDevelopment) {
+        window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+    } else {
+        window.loadURL(
+            formatUrl({
+                pathname: path.join(__dirname, "index.html"),
+                protocol: "file",
+                slashes: true,
+            })
+        );
+    }
+
+    window.on("closed", () => {
+        mainWindow = null;
+    });
+
+    window.webContents.on("devtools-opened", () => {
+        window.focus();
+        setImmediate(() => {
+            window.focus();
+        });
+    });
+
+    return window;
 }
 
-// quit application when all windows are closed
 app.on("window-all-closed", () => {
-  // on macOS it is common for applications to stay open until the user explicitly quits
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
-  if (mainWindow === null) {
-    mainWindow = createMainWindow();
-  }
+    if (mainWindow === null) {
+        mainWindow = createMainWindow();
+    }
 });
 
-// create main BrowserWindow when electron is ready
 app.on("ready", () => {
-  mainWindow = createMainWindow();
+    mainWindow = createMainWindow();
 });
 
 if (module.hot) {
-  module.hot.accept();
+    module.hot.accept();
 }
---
+++ src/renderer/app/App.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./App.css";
+import { hot } from "react-hot-loader/root";
+
+function App() {
+    const [count, setCount] = useState<number>(0);
+    return (
+        <div className="App">
+            <header className="App-header">
+                count : {count}
+                <button onClick={() => {setCount((c) => c + 1);}}>+</button>
+                
+                <button onClick={() => {setCount((c) => c - 1);}}>-</button>
+                
+                <p>Hello world</p>
+            </header>
+        </div>
+    );
+}
+
+export default hot(App);
